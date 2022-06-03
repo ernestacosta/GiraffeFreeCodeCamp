a1014788383b60e332ff0ae57253a4080ecd131c
Ernest Acosta <ernestacosta@icloud.com>
2022-06-02 19:52:41 -0700

    initial commit

---
+++ Giraffe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Giraffe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Tom";
+            int characterAge = 25; 
+
+            Console.WriteLine($"There once was a man named {characterName}");
+            Console.WriteLine($"He was {characterAge} years old");
+
+            characterName = "Mike";                    //Ability to change names mid story
+            Console.WriteLine($"He really liked the name {characterName}");
+            Console.WriteLine($"But he didn't like being {characterAge}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
